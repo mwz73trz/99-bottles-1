f704480b4b8f2fbe36630f55bf857564abca0894
mwz73trz <michaelzura@bellsouth.net>
2021-09-30 09:44:56 -0400

    completed in js/py

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,29 @@
-function bottleSong() {
-  // Write your code here!
-};
+function bottleSong(num) {
+  let numBottles = num;
+  while (numBottles >= 1) {
+    console.log(
+      `${numBottles} bottle${
+        numBottles > 1 ? "s" : ""
+      } of beer on the wall, ${numBottles} bottle${
+        numBottles > 1 ? "s" : ""
+      } of beer.`
+    );
+    numBottles--;
+    if (numBottles > 0) {
+      console.log(
+        `Take one down and pass it around, ${numBottles} bottle${
+          numBottles > 1 ? "s" : ""
+        } of beer on the wall.`
+      );
+    }
+  }
+  console.log(
+    "Take one down and pass it around, no more bottles of beer on the wall."
+  );
+  console.log("No more bottles of beer on the wall, no more bottles of beer.");
+  console.log(
+    `Go to the store and buy some more, ${num} bottles of beer on the wall.`
+  );
+}
 
-bottleSong();
+console.log(bottleSong(10));
--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,22 @@
-def bottle_song():
-	# write your code here!
+def bottle_song(num):
+	total = num
+	while num > 2:
+		print(f"{num} bottles of beer on the wall, {num} bottles of beer.")
+		print("Take one down and pass it around,")
+		num -= 1
+		print(f"{num} bottles of beer on the wall.")
+	while num == 2:
+		print(f"{num} bottles of beer on the wall, {num} bottles of beer.")
+		print("Take one down and pass it around,")
+		num -= 1
+		print(f"{num} bottle of beer on the wall.")
+	while num == 1:
+		print(f"{num} bottle of beer on the wall, {num} bottle of beer.")
+		print("Take one down and pass it around,")
+		num -= 1
+		print("No more bottles of beer on the wall.") 
+		print("No more bottles of beer on the wall, no more bottles of beer.")
+		print(f"Go to the store and buy some more, {total} bottles of beer on the wall.")
 
 
-bottle_song()
+bottle_song(5)
